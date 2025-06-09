b9611f82270ee6a0f6d9c53920ad8fda60b83c44
Maxim Khan-Magomedov <maxim.km@gmail.com>
2025-06-09 03:37:19 +0300

    Use localized strings in post list

--- app/models/post.rb
+++ app/models/post.rb
@@ -30,7 +30,7 @@ class Post < ApplicationRecord
             length: { maximum: 250 }
 
   scope :ordered, -> { order(date: :desc, sibling_id: :asc) }
-  scope :list, ->(page = 1) { ordered.page(page) }
+  scope :list, ->(page = 1) { ordered.includes(:sibling).page(page) }
 
   def self.permitted_params
     %i[body date image sibling_id title]
